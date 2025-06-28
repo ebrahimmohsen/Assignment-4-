c23c41f5185b73cc04475e850c0d9b633e32c2c3
ebrahimmohsen <ebrahimzaki456@gmail.com>
2025-06-29 02:10:08 +0300

    Q1 solved

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1 Divisible by 3 and 4
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine((num % 3 == 0 && num % 4 == 0) ? "Yes" : "No");
+            #endregion
+
         }
     }
 }
